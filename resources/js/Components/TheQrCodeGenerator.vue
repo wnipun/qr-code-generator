8219f67307e267070d52576e3e8d659c83294768
Nipun Perera <nipun5perera@gmail.com>
2024-07-09 20:04:38 +1000

    feat: add qr code download button

--- resources/js/Components/TheQrCodeGenerator.vue
+++ resources/js/Components/TheQrCodeGenerator.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import QrCodeVue from 'qrcode.vue'
+import { Field } from '@ark-ui/vue'
 
 const qrValue = ref('https://qrcodegenerator.tech')
 const aqSize = ref(150)
@@ -12,10 +13,28 @@ const generate = () => {
 
     if (textInput.value == '') {
         inputEmpty.value = true
+        console.log('empty');
         return
     }
 
     qrValue.value = textInput.value
+}
+
+const download = () => {
+    let canvasImage = document.getElementById('qr-code-canvas').toDataURL('image/png');
+    let xhr = new XMLHttpRequest();
+    xhr.responseType = 'blob';
+    xhr.onload = function () {
+        let a = document.createElement('a');
+        a.href = window.URL.createObjectURL(xhr.response);
+        a.download = 'qrcode.png';
+        a.style.display = 'none';
+        document.body.appendChild(a);
+        a.click();
+        a.remove();
+    };
+    xhr.open('GET', canvasImage);
+    xhr.send();
 }
 </script>
 <template>
@@ -25,6 +44,7 @@ const generate = () => {
                 :value="qrValue"
                 class="border border-gray-200 rounded-md overflow-hidden p-[0.5rem] mx-auto"
                 :size="aqSize"
+                id="qr-code-canvas"
             ></QrCodeVue>
         </div>
         <div class="border-t-2 border-gray-200 pt-[1rem]">
@@ -33,10 +53,24 @@ const generate = () => {
                 :class="{
                     'outline-red-500 outline': inputEmpty
                 }"
+                @keyup.meta.enter="generate()"
                 class="border-gray-100 border block p-[0.25rem] w-full min-h-[5rem] text-sm leading-tight"
             >
             </textarea>
-            <button @click.prevent="generate()" class="bg-blue-700 text-white font-bold px-[1rem] py-[0.5rem] rounded-[0.25rem] mt-[1rem] mr-auto ml-0">Generate</button>
+            <div class="flex items-center gap-[1rem] justify-center mt-[1rem]">
+                <button
+                    @click.prevent="generate()"
+                    class="bg-blue-700 text-white font-bold px-[1rem] py-[0.5rem] rounded-[0.25rem] hover:bg-blue-600 transition-all duration-500 ease-in-out"
+                >
+                    Generate
+                </button>
+                <button
+                    @click.prevent="download()"
+                    class="bg-green-700 hover:bg-green-600 transition-all duration-500 ease-in-out text-white font-bold px-[1rem] py-[0.5rem] rounded-[0.25rem]"
+                >
+                    Download
+                </button>
+            </div>
         </div>
     </div>
 </template>
